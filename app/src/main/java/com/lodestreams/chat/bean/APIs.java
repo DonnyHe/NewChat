e23c8e3f213d798762e2240030a560e898bba8d0
DonnyHe <hjytl@qq.com>
2016-11-30 09:15:56 +0800

    更换稳定的服务器

--- app/src/main/java/com/lodestreams/chat/bean/APIs.java
+++ app/src/main/java/com/lodestreams/chat/bean/APIs.java
@@ -5,7 +5,7 @@ package com.lodestreams.chat.bean;
  */
 public class APIs {
     //private final static String SERVER_ADDRRESS = "http://chat.lodestreams.io:1337";
-    private final static String SERVER_ADDRRESS = "http://kr.lodestreams.io:1337";
+    private final static String SERVER_ADDRRESS = "http://110.10.176.61:1337";
     //private final static String SERVER_ADDRRESS = "http://192.168.11.141:1337";
     public final static String API_ROOM = SERVER_ADDRRESS + "/rooms";
 }
